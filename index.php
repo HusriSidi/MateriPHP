0716698e8861552b08b4225690caa260e5418523
HusriSidi <95020878+HusriSidi@users.noreply.github.com>
2021-11-27 10:00:27 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+  <meta charset="utf-8">
+    <title>BELAJAR PHP</title>
+  </head>
+  <body>
+
+  <?php
+  $nama = "Belajar Dasar Dasar PHP";
+  $_123 = "Punya Saya";
+
+  echo "<h1>$_123</h1>";
+  echo "Halo ". $nama."<br>";
+  echo "Member";
+  ?>
+  </body>
+</html>
